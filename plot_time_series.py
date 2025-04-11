a5439a0e20197afbaf563d8837e645f1f09c5000
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-04-11 19:16:59 +0200

    Update plot_time_series.py

--- plot_time_series.py
+++ plot_time_series.py
@@ -23,11 +23,12 @@ def plot_times_series(file):
     fig, axs = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
     
     # Raster Plots for Ia afferent fibers
-    df=df.loc[1,'Time']-df.loc[0, 'Time']
+    dt=data.loc[1,'Time']-data.loc[0, 'Time']
     for i, type_fiber in enumerate(['Ia', 'II', 'motoneuron']):
       spike_matrix = data.filter(like=f'spikes_times_{type_fiber}').to_numpy()
-      rows, cols = np.nonzero(spike_matrix)
-      axs[i].plot(cols * dt, rows, 'k.', markersize=3)
+      print('spike_matrix', spike_matrix.shape)
+      rows, _ = np.nonzero(spike_matrix)
+      axs[i].plot(rows * dt, i*np.ones((len(rows))), 'k.', markersize=3)
       axs[i].set(title=f"{type_fiber} Spike Raster Plot", ylabel="Neuron Index")
       axs[i].grid(True)
     
@@ -38,23 +39,17 @@ def plot_times_series(file):
 
     # Plot activations
     # Automatically find all activation columns for motor neurons
-    activation_cols = [col for col in data.columns if col.startswith('activation_motor_')]
-    n_motors = len(activation_cols)
-
+    activation_cols = [col for col in data.columns if col.startswith('activation_')]
+    print('data columns', data.columns)
+    print('activation_cols', activation_cols)
     # Create subplots: one for each motor neuron + one for mean activation
-    fig, axs = plt.subplots(n_motors + 1, 1, figsize=(10, 2.5 * (n_motors + 1)), sharex=True)
+    fig, axs_ = plt.subplots(len(activation_cols), 1, figsize=(10, 1.5 * (len(activation_cols))), sharex=True)
 
     # Plot individual motor neuron activations
     for i, col in enumerate(activation_cols):
-        axs[i].plot(data['time'], data[col], label=col.replace('_', ' ').capitalize())
-        axs[i].set_ylabel('Activation')
-        axs[i].legend(loc='upper right')
-
-    # Plot mean activation
-    axs[-1].plot(data['time'], data['mean_activation'], label='Mean activation', color='black')
-    axs[-1].set_xlabel('Time (s)')
-    axs[-1].set_ylabel('Activation')
-    axs[-1].legend(loc='upper right')
+        axs_[i].plot(data['Time'], data[col], label=col.replace('_', ' ').capitalize())
+        axs_[i].set_ylabel('Activation')
+        axs_[i].legend(loc='upper right')
 
     plt.tight_layout()
     plt.savefig('Activation.png')
