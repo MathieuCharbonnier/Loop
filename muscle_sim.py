4f754e1a7e98beeab830809f14a6e27acc81e046
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-04-12 15:42:48 +0200

    Update muscle_sim.py

--- muscle_sim.py
+++ muscle_sim.py
@@ -1,12 +1,12 @@
-# muscle_sim.py
 import argparse
 import numpy as np
+import sys
 import opensim as osim
 
-def run_simulation(dt, T, activation_array, output_name):
+def run_simulation(dt, T, muscle_name, initial_fiber_length, activation_array):
     model = osim.Model("Model/gait2392_millard2012_pelvislocked.osim")
-    muscle_name = "iliacus_r"
-    time_array=np.arange(0,T, dt)
+    
+    time_array = np.arange(0, T, dt)
 
     class ActivationController(osim.PrescribedController):
         def __init__(self, model, muscle_name, time_array, activation_array):
@@ -29,10 +29,10 @@ def run_simulation(dt, T, activation_array, output_name):
     reporter.set_report_time_interval(dt)
     muscle = model.getMuscles().get(muscle_name)
     reporter.addToReport(muscle.getOutput("fiber_length"), "fiber_length")
-    reporter.addToReport(muscle.getOutput("fiber_velocity"), "fiber_velocity")
     model.addComponent(reporter)
 
     state = model.initSystem()
+    muscle.setFiberLength(state, initial_fiber_length)
     manager = osim.Manager(model)
     state.setTime(0)
     manager.setIntegratorAccuracy(1e-4)
@@ -40,23 +40,25 @@ def run_simulation(dt, T, activation_array, output_name):
     manager.integrate(T)
 
     results_table = reporter.getTable()
-    osim.STOFileAdapter.write(results_table, output_name)
-    print(f"Simulation complete. Data saved to {output_name}.")
+    return results_table.getDependentColumn("fiber_length").to_numpy()
 
-
+   
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dt", type=float, required=True, help="Time interval")
-    parser.add_argument("--T", type=float, required=True, help="Final time")
-    parser.add_argument("--activation", type=str, required=True, help="Path to activation .npy file")
-    parser.add_argument("--output", type=str, required=True, help="Name of the output file")
+    
+    parser = argparse.ArgumentParser(description='Muscle simulation')
+    parser.add_argument('--dt', type=float, required=True, help='Time step')
+    parser.add_argument('--T', type=float, required=True, help='Total simulation time')
+    parser.add_argument('--muscle', type=str, required=True, help='Muscle name')
+    parser.add_argument('--L', type=float, required=True, help='initial fiber length')
+    parser.add_argument('--input_file', type=str, required=True, help='Path to input numpy array file')
+    parser.add_argument('--output_file', type=str, required=True, help='Path to save output numpy array')
+    
     args = parser.parse_args()
-
+    
+    # Load the input array
+    activation_array = np.load(args.input_file)
+    # Run the simulation
+    fiber_length=run_simulation(args.dt, args.T, args.muscle, args.L, activation_array)
+    # Save the result to the output file
+    np.save(args.output_file, fiber_length)
   
-    activation_array = np.load(args.activation)
-    output_name=args.output
-    dt=args.dt
-    T=args.T
-
-
-    run_simulation(dt, T, activation_array, output_name)
