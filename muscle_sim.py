bcdddd04554a4633d8a5c3f9f10ad40fb6d8b488
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-04-13 15:30:45 +0200

    Update muscle_sim.py

--- muscle_sim.py
+++ muscle_sim.py
@@ -3,9 +3,9 @@ import numpy as np
 import sys
 import os
 import opensim as osim
-from opensim import Millard2012AccelerationMuscle
 
-def run_simulation(dt, T, muscle_name, activation_array,resume_from=None, L0=None, v0=None):
+def run_simulation(dt, T, muscle_name, activation_array,resume_from=None):
+
     model = osim.Model("Model/gait2392_millard2012_pelvislocked.osim")
     
     time_array = np.arange(0, T, dt)
@@ -68,12 +68,6 @@ def run_simulation(dt, T, muscle_name, activation_array,resume_from=None, L0=Non
             # If loading fails, use the initialized state
             pass
 
-    """
-    if L0  is not None:
-        muscle.setFiberLength(state, L0)
-    if v0 is not None:
-        muscle.setFiberVelocity(state, v0)
-    """
     manager = osim.Manager(model)
     manager.setIntegratorAccuracy(1e-4)
     manager.initialize(state)
@@ -97,8 +91,6 @@ if __name__ == "__main__":
     parser.add_argument('--input_file', type=str, required=True, help='Path to input numpy array file')
     parser.add_argument('--output_file', type=str, required=True, help='Path to save output numpy array')
     parser.add_argument('--resume_from', type=str, help='initialize the system with the state.sto file')
-    parser.add_argument('--L0', type=float, help='Initial fiber length')
-    parser.add_argument('--v0', type=float, help='Initial fiber velocity')
     
     args = parser.parse_args()
     
@@ -106,10 +98,6 @@ if __name__ == "__main__":
     activation_array = np.load(args.input_file)
 
     optional_args = {}
-    if args.L0 is not None:
-        optional_args["L0"] = args.L0
-    if args.v0 is not None:
-        optional_args["v0"] = args.v0
     if args.resume_from is not None:
         optional_args["resume_from"]=args.resume_from
 
