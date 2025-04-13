e1bf3ef4026d22eacc266e631cea83e4305d6d2f
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-04-13 11:35:15 +0200

    Update muscle_sim.py

--- muscle_sim.py
+++ muscle_sim.py
@@ -3,9 +3,9 @@ import numpy as np
 import sys
 import os
 import opensim as osim
-from opensim import Millard2012EquilibriumMuscle
+from opensim import Millard2012AccelerationMuscle
 
-def run_simulation(dt, T, muscle_name, activation_array, continue_from=None, save_state=None):
+def run_simulation(dt, T, muscle_name, activation_array, L0=None, v0=None):
     model = osim.Model("Model/gait2392_millard2012_pelvislocked.osim")
     
     time_array = np.arange(0, T, dt)
@@ -30,34 +30,24 @@ def run_simulation(dt, T, muscle_name, activation_array, continue_from=None, sav
     reporter.setName("MuscleReporter")
     reporter.set_report_time_interval(dt)
     muscle = model.getMuscles().get(muscle_name)
-    muscle = Millard2012EquilibriumMuscle.safeDownCast(muscle)
+    muscle = Millard2012AccelerationMuscle.safeDownCast(muscle)
     reporter.addToReport(muscle.getOutput("fiber_length"), "fiber_length")
     model.addComponent(reporter)
 
     state = model.initSystem()
-    
-    # Initialize from a saved state if provided
-    if continue_from and os.path.exists(continue_from):
-        print(f"Loading simulation state from {continue_from}")
-        sto = osim.Storage(continue_from)
-        state = osim.StatesTrajectory.createFromStatesStorage(model, sto)
-        T += state.getTime()  # Add the starting time to the total simulation time
+    state.setTime(0)
 
-    else:
-        state.setTime(0)
-    
+    if L0  is not None:
+        muscle.setFiberLength(state, L0)
+    if v0 is not None:
+        muscle.setFiberVelocity(state, v0)
+
     manager = osim.Manager(model)
     manager.setIntegratorAccuracy(1e-4)
     manager.initialize(state)
     
     manager.integrate(T)
-    
-    # Save the final state if requested
-    if save_state:
-        state = manager.getState()
-        state.write(save_state)
-        print(f"Simulation state saved to {save_state}")
-    
+
     results_table = reporter.getTable()
     return results_table.getDependentColumn("fiber_length").to_numpy()
 
@@ -70,22 +60,27 @@ if __name__ == "__main__":
     parser.add_argument('--muscle', type=str, required=True, help='Muscle name')
     parser.add_argument('--input_file', type=str, required=True, help='Path to input numpy array file')
     parser.add_argument('--output_file', type=str, required=True, help='Path to save output numpy array')
-    parser.add_argument('--continue_from', type=str, help='Path to state file to continue simulation from')
-    parser.add_argument('--save_state', type=str, help='Path to save final simulation state')
+    parser.add_argument('--L0', type=float, help='Initial fiber length')
+    parser.add_argument('--v0', type=float, help='Initial fiber velocity')
     
     args = parser.parse_args()
     
     # Load the input array
     activation_array = np.load(args.input_file)
-    
+
+    optional_args = {}
+    if args.L0 is not None:
+        optional_args["L0"] = args.L0
+    if args.v0 is not None:
+        optional_args["v0"] = args.v0
+
     # Run the simulation
     fiber_length = run_simulation(
         args.dt, 
         args.T, 
         args.muscle, 
         activation_array,
-        continue_from=args.continue_from,
-        save_state=args.save_state
+        **optional_args
     )
     
     # Save the result to the output file
