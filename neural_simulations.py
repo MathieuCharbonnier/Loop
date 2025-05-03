985bc7dbc5b79a022e77832436ca2aadd4d2c576
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-03 11:02:22 +0200

    Update neural_simulations.py

--- neural_simulations.py
+++ neural_simulations.py
@@ -194,11 +194,11 @@ def run_flexor_extensor_neuron_simulation(stretch, velocity,
 
     mon_v_moto_flexor=StateMonitor(moto, 'v', 20)
     mon_gIa_moto_flexor=StateMonitor(moto, 'gIa', 20)
-    mon_gex_moto_flexor=StateMonitor(moto 'gex', 20)
+    mon_gex_moto_flexor=StateMonitor(moto, 'gex', 20)
     mon_gi_moto_flexor=StateMonitor(moto, 'gIa', 20)
     mon_v_moto_extensor=StateMonitor(moto, 'v', 80)
     mon_gIa_moto_extensor=StateMonitor(moto, 'gIa', 80)
-    mon_gex_moto_extensor=StateMonitor(moto 'gex', 80)
+    mon_gex_moto_extensor=StateMonitor(moto, 'gex', 80)
     mon_gi_moto_extensor=StateMonitor(moto, 'gIa', 80)
     
     plt.title(" Motoneurons Voltage")
