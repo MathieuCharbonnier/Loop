249b9dee6f713cc59e5063a3228b7048f36dd546
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-11 22:21:40 +0200

    position legend

--- stimulation_analysis.py
+++ stimulation_analysis.py
@@ -164,7 +164,7 @@ def EES_stim_analysis(
                                        '.', markersize=4, color=color)
                         
                         # Add a label for this muscle at its position
-                        ax.text(0.01+ idx*0.9, 1.05, muscle_name, 
+                        ax.text(0.01+ idx*0.09, 1.05, muscle_name, 
                                transform=ax.get_xaxis_transform(), color=color,
                                fontweight='bold', verticalalignment='center')
                 
