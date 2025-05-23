fe9bb2f624f06c4051e53f06276578408246bb28
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-23 17:35:47 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from .SpecializedBiologicalSystems import Monosynaptic, Disynaptic, ReciprocalInhibition, SpinalCircuitWithIb
-from .Analyser import Analyzer, from .SpecializedAnalyser import SpecializedAnalyser
+from .Analyser import Analyzer
+from .Controller import Controller
 __all__ = [
     "Monosynaptic",
     "Disynaptic",
     "ReciprocalInhibition",
     "SpinalCircuitWithIb",
     "Analyzer",
-    "SpecializedAnalyser"
+    "Controller"
 ]
