80d58a4b14967a24ddbcebe14eeda1a63e04b220
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-23 15:45:26 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 from .SpecializedBiologicalSystems import Monosynaptic, Disynaptic, ReciprocalInhibition, SpinalCircuitWithIb
-
+from .Analyser import Analyzer
 __all__ = [
     "Monosynaptic",
     "Disynaptic",
     "ReciprocalInhibition",
-    "SpinalCircuitWithIb"
+    "SpinalCircuitWithIb",
+    "Analyzer"
 ]
