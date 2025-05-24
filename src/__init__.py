d86d47f55aea814e98ff42b5517c7aad4e254392
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-25 00:34:47 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
-from .BiologicalSystems import Monosynaptic, Disynaptic, ReciprocalInhibition, SpinalCircuitWithIb
+from .SpecializedBiologicalSystems.monosynaptic import Monosynaptic
+from .SpecializedBiologicalSystems.disynaptic import Disynaptic
+from .SpecializedBiologicalSystems.reciprocal_inhibition import ReciprocalInhibition
+from .SpecializedBiologicalSystems.spinal_circuit_with_ib import SpinalCircuitWithIb
 from .Analyzer import Analyzer
 from .Controller import Controller
 __all__ = [
