288be6d741f00c7fb1a301df1dc232789613e936
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-05-29 12:30:37 +0200

    Update and rename AnalyzerReflex.py to ReflexAnalyzer.py

--- src/Analyzers/ReflexAnalyzer.py
+++ src/Analyzers/ReflexAnalyzer.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from ..BiologicalSystems.BiologicalSystem import BiologicalSystem
 
 
-class AnalyzerReflex:
+class ReflexAnalyzer:
     """
     Analyzer for reflex behavior under different parameter variations.
     
