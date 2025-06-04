acc5c2f16976683efcc0b45cac626e95ce0f31fc
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-06-04 17:28:40 +0200

    Update Disynaptic.py

--- src/BiologicalSystems/Disynaptic.py
+++ src/BiologicalSystems/Disynaptic.py
@@ -16,7 +16,7 @@ class Disynaptic(BiologicalSystem):
                  associated_joint="ankle_angle_r", neurons_population=None, connections=None, 
                  spindle_model=None, ees_recruitment_profile=None, fast_type_mu=True, 
                  initial_state_neurons=None, initial_condition_spike_activation=None, 
-                 initial_state_opensim=None, activation_funct=None, stretch_history_func=None):
+                 initial_state_opensim=None, activation_funct=None, stretch_history_func=None, seed=41):
         """
         Initialize a disynaptic reflex system without Ib pathway.
         """
@@ -89,7 +89,7 @@ class Disynaptic(BiologicalSystem):
                         muscles_names, associated_joint, fast_type_mu,
                         neurons_population, connections, spindle_model, 
                         initial_state_neurons, initial_condition_spike_activation, 
-                        initial_state_opensim, activation_funct, stretch_history_func)
+                        initial_state_opensim, activation_funct, stretch_history_func, seed)
         
         self.validate_input()
 
