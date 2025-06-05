b34ae20e8df46f55c497f56a9b650d0a764aa11f
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-06-05 17:43:55 +0200

    Update BiMuscles.py

--- src/BiologicalSystems/BiMuscles.py
+++ src/BiologicalSystems/BiMuscles.py
@@ -149,9 +149,9 @@ class BiMuscles(BiologicalSystem):
 
         super().__init__(reaction_time, ees_recruitment_profile, biophysical_params, 
                         muscles_names, associated_joint, fast_type_mu,
-                        neurons_population, connections, spindle_model, 
+                        neurons_population, connections, spindle_model,seed, 
                         initial_state_neurons, initial_condition_spike_activation, 
-                        initial_state_opensim, activation_funct, stretch_history_func, seed)  
+                        initial_state_opensim, activation_funct, stretch_history_func)  
 
         # Validate the configuration
         self.validate_input()
