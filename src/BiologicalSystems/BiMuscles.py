382d3eece8ee43f0a3a51b370b86ef5a00779bd9
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-06-08 16:06:16 +0200

    Update BiMuscles.py

--- src/BiologicalSystems/BiMuscles.py
+++ src/BiologicalSystems/BiMuscles.py
@@ -113,8 +113,8 @@ class BiMuscles(BiologicalSystem):
             
         if spindle_model is None:
             spindle_model = {
-                "Ia": "10+ 2*stretch + 4.3*sign(stretch_velocity)*abs(stretch_velocity)**0.6",
-                "II": "20 + 13.5*stretch_delay",
+                "Ia": "10+ 0.4*stretch + 0.86*sign(stretch_velocity)*abs(stretch_velocity)**0.6",
+                "II": "20 + 3.375*stretch_delay",
                 "Ia_II_delta_delay": 20*ms
             }
             
