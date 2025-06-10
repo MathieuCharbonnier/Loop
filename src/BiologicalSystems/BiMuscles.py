852cbd9659d2a3065e2f9ea2c240a2c9b1e229ba
MathieuCharbonnier <95469477+MathieuCharbonnier@users.noreply.github.com>
2025-06-10 13:58:25 +0200

    Update BiMuscles.py

--- src/BiologicalSystems/BiMuscles.py
+++ src/BiologicalSystems/BiMuscles.py
@@ -52,7 +52,7 @@ class BiMuscles(BiologicalSystem):
                 'E_ex': 0*mV,
                 'tau_e': 0.5*ms,
                 'E_inh': -75*mV,
-                'tau_i': 2.8*ms,
+                'tau_i': 2.5*ms,
                 'threshold_v': -50*mV
             }
             
